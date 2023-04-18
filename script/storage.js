9718b748f34655dc1cf07e62993666d30bbc30fc
kylehuynh0811 <huynhphananhkiet.tphcm@gmail.com>
2023-04-18 14:57:05 +0700

    damn

--- script/storage.js
+++ script/storage.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+//Toggle active class for sidebar Animation
+const navEl = document.getElementById("sidebar");
+
+navEl.addEventListener("click", function () {
+  this.classList.toggle("active");
+});
+
+//Save to local storage
+function saveToStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+//get from local storage
+function getFromStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}

455e4c587abc025fb6a45840ce2b1743a1c84ee3
Kiet Huynh <huynhphananhkiet.tphcm@gmail.com>
2023-06-06 12:12:39 +0000

    Finishing the Data section

--- script/data.js
+++ script/data.js
@@ -18,7 +18,7 @@ btnExport.addEventListener("click", () => {
 //Export function
 const saveStaticDatatoFile = () => {
   //create data for saving to the file
-  let blob = new Blob([JSON.stringify(getFromStorage("petArr"), null, 2)], {
+  var blob = new Blob([JSON.stringify(getFromStorage("petArr"),null,2)], {
     type: "application/json",
   });
   saveAs(blob, "petData.json");
@@ -66,11 +66,11 @@ const checkFile = (data) => {
     alert("Invalid file: the file does not contain an array that store objects")
     return false;
   }
-
+  //Check if the data is a Pet object
   if (!isPetObject) {
     return false;
   }
-
+  //Check if the data is in a valid format
   if (!isValidate) {
     alert("Invalid file: Objects in the file does not match the required format")
     return false;
@@ -78,14 +78,13 @@ const checkFile = (data) => {
 
   return true;
 }
-
+//Check if the data is a Pet object
 const isPetObject = (data) => {
-
   if(!data.every((item) => item instanceof Object)){
     alert("Invalid file: some elements in the array are not objects");
     return false;
   }
-
+  //Check if any Object miss any properties
   const isOk = date.every((item) => {
     return (
       Object.keys(item).length === 12 &&
@@ -113,5 +112,85 @@ const isPetObject = (data) => {
 }
 
 const isValidate = (data) => {
+  //Check for the validity of an object properties
+  return data.every( (pet) => {
+    if (pet.id.trim().length === 0){
+      alert("Invalid file: File contain invalid ID");
+      return false;
+    }
 
+    if (pet.name.trim().length === 0){
+      alert("Invalid file: File contains invalid Name");
+      return false;
+    }
+
+    pet.age = parseInt(pet.age)
+    if (pet.age.trim().length === 0){
+      alert("Invalid file: File contains invalid ID");
+      return false;
+    }
+
+    pet.weight = parseInt(pet.weight)
+    if (pet.weight.trim().length === 0){
+      alert("Invalid file: File contains invalid Weight");
+      return false;
+    }
+
+    pet.length = parseInt(pet.length)
+    if (pet.length.trim().length === 0){
+      alert("Invalid file: File contains invalid Length");
+      return false;
+    }
+
+    if (pet.type.trim().length === 0){
+      alert("Invalid file: File contains invalid Type");
+      return false;
+    }
+
+    if (pet.color.trim().length=== 0){
+      alert("Invalid file: File contains invalid Color");
+      return false;
+    }
+
+    if (pet.breed.trim().length === 0){
+      alert("Invalid file: File contains invalid Breed");
+      return false;
+    }
+
+    if (pet.date.trim().length === 0){
+      alert("Invalid file: File contains invalid Date");
+      return false;
+    }
+
+    if (typeof pet.vaccinated !== 'boolean'){
+      alert("Invalid file: File contain invalid Vaccinated");
+      return false;
+    }
+
+    if (typeof pet.dewormed !== 'boolean'){
+      alert("Invalid file: File contain invalid Dewormed");
+      return false;
+    }
+
+    if (typeof pet.sterilized !== 'boolean'){
+      alert("Invalid file: File contain invalid Sterilized");
+      return false;
+    }
+    //Check IDs for uniqueness
+    let count = 1;
+
+    for (let item of data){
+      if (pet.id === item.id){
+        //Duplicated ID
+        if (count > 1) {
+          alert("Invalid file: One or some pet IDs are duplicated");
+          return false;
+        }
+        count++;
+      }
+    }
+
+    return true;
+  }
+  )
 }
